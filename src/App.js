79bab9858bd71ec7560c02624a8c31f7d2d54085
unknown <you@example.com>
2021-05-24 21:44:45 +0700

    add input image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, {useState, useRef} from 'react';
-import {Container, Card, CardContent, makeStyles, Grid, TextField, Button} from '@material-ui/core';
+import React, { useState, useRef } from 'react';
+import { Container, Card, CardContent, makeStyles, Grid, TextField, Button } from '@material-ui/core';
 import QRCode from 'qrcode';
 import QrReader from 'react-qr-reader';
 
 
-function App() { 
+function App() {
   const [text, setText] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [scanResultFile, setScanResultFile] = useState('');
-  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+  const [scanResultWebCam, setScanResultWebCam] = useState('');
   const classes = useStyles();
   const qrRef = useRef(null);
 
 
   const generateQrCode = async () => {
     try {
-          const response = await QRCode.toDataURL(text);
-          setImageUrl(response);
-    }catch (error) {
+      const response = await QRCode.toDataURL(text);
+      setImageUrl(response);
+    } catch (error) {
       console.log(error);
     }
   }
@@ -25,9 +25,9 @@ function App() {
     console.log(error);
   }
   const handleScanFile = (result) => {
-      if (result) {
-          setScanResultFile(result);
-      }
+    if (result) {
+      setScanResultFile(result);
+    }
   }
   const onScanFile = () => {
     qrRef.current.openImageDialog();
@@ -36,48 +36,60 @@ function App() {
     console.log(error);
   }
   const handleScanWebCam = (result) => {
-    if (result){
-        setScanResultWebCam(result);
+    if (result) {
+      setScanResultWebCam(result);
     }
-   }
+  }
   return (
     <Container className={classes.conatiner}>
-          <Card>
-              <h2 className={classes.title}>Generate Download & Scan QR Code with React js</h2>
-              <CardContent>
-                  <Grid container spacing={2}>
-                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                         <h3>Qr Code Scan by Web Cam</h3>
-                         <QrReader
-                         delay={300}
-                         style={{width: '100%'}}
-                         onError={handleErrorWebCam}
-                         onScan={handleScanWebCam}
-                         />
-                         <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
-                      </Grid>
-                  </Grid>
-              </CardContent>
-          </Card>
+      <Card>
+        <h2 className={classes.title}>Generate Download & Scan QR Code with React js</h2>
+        <CardContent>
+          <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+            <Button className={classes.btn} variant="contained" color="secondary" onClick={onScanFile}>Input QR Code</Button>
+            <QrReader
+              ref={qrRef}
+              delay={300}
+              style={{ width: '100%' }}
+              onError={handleErrorFile}
+              onScan={handleScanFile}
+              legacyMode
+            />
+            <h3>Scanned Code: {scanResultFile}</h3>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+              <h3>Qr Code Scan by Web Cam</h3>
+              <QrReader
+                delay={300}
+                style={{ width: '100%' }}
+                onError={handleErrorWebCam}
+                onScan={handleScanWebCam}
+              />
+              <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </Container>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
-    conatiner: {
-      marginTop: 10
-    },
-    title: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems:  'center',
-      background: '#3f51b5',
-      color: '#fff',
-      padding: 20
-    },
-    btn : {
-      marginTop: 10,
-      marginBottom: 20
-    }
+  conatiner: {
+    marginTop: 10
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#3f51b5',
+    color: '#fff',
+    padding: 20
+  },
+  btn: {
+    marginTop: 10,
+    marginBottom: 20
+  }
 }));
 export default App;
